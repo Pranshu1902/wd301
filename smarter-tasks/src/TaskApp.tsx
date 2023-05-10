443f2291f8803db7dee1ad1d5ee6e6aa30b3497c
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-05-10 21:21:48 +0530

    completed L4 milestone

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -8,56 +8,42 @@ interface TaskAppProp {}
 interface TaskAppState {
   tasks: TaskItem[];
 }
-class TaskApp extends React.Component<TaskAppProp, TaskAppState> {
-  constructor(props: TaskAppProp) {
-    super(props);
-    this.state = {
+
+React.useState<TaskAppState>;
+const TaskApp = (props: TaskAppProp) => {
+  const [taskAppState, setTaskAppState] = useLocalStorage<TaskAppState>(
+    "tasks",
+    {
       tasks: [],
-    };
-  }
+    }
+  );
 
-  // const [taskAppState, setTaskAppState] = useLocalStorage<TaskAppState>(key: "tasks",{
-  //   tasks: [],
-  // });  
+  const addTask = (task: TaskItem) => {
+    setTaskAppState({ tasks: [...taskAppState.tasks, task] });
+  };
 
-  addTask = (task: TaskItem) => {
-    this.setState((state, props) => {
-      return {
-        tasks: [...state.tasks, task],
-      };
+  const deleteTask = (task: TaskItem) => {
+    setTaskAppState({
+      tasks: taskAppState.tasks.filter((t) => t !== task),
     });
   };
 
-  deleteTask = (task: TaskItem) => {
-    this.setState((state, props) => {
-      return {
-        tasks: state.tasks.filter((t) => t !== task),
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div className="container py-10 max-w-4xl mx-auto">
-        <h1 className="text-3xl mb-2 font-bold text-slate-700">
-          Smarter Tasks
-        </h1>
-        <h1 className="text-md mb-6 text-slate-600">
-          <span className="font-bold">Project: </span>
-          Graduation Final Year Project (Revamp college website)
-        </h1>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="border border-slate-200 rounded-xl p-4">
-            <h1 className="text-slate-500 font-bold text-center mb-2">
-              Pending
-            </h1>
-            <TaskForm addTask={this.addTask} />
-            <TaskList tasks={this.state.tasks} removeTask={this.deleteTask} />
-          </div>
+  return (
+    <div className="container py-10 max-w-4xl mx-auto">
+      <h1 className="text-3xl mb-2 font-bold text-slate-700">Smarter Tasks</h1>
+      <h1 className="text-md mb-6 text-slate-600">
+        <span className="font-bold">Project: </span>
+        Graduation Final Year Project (Revamp college website)
+      </h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="border border-slate-200 rounded-xl p-4">
+          <h1 className="text-slate-500 font-bold text-center mb-2">Pending</h1>
+          <TaskForm addTask={addTask} />
+          <TaskList tasks={taskAppState.tasks} removeTask={deleteTask} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TaskApp;
